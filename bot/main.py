5886ad4aa1e98508adcf7e97272e77cd4b9e9cc3
tenessy0570 <tenessy0570@gmail.com>
2023-02-26 00:23:36 +0700

    refactor

--- bot/main.py
+++ bot/main.py
@@ -5,8 +5,6 @@ from aiogram import types
 
 import config
 from bot import utils
-from file_converter.file_converter import FileConverter
-from speech_converter.speech_converter import SpeechConverter
 
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -15,29 +13,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(content_types=["voice"])
 async def speech_to_text(message: types.Message):
     voice = message.voice
-
-    # Convert to aiff so file can be read by speech-recognition
-    new_format = ".aiff"
-
-    unconverted_file_path = utils.resolve_new_unconverted_file_path(
-        new_format=new_format
-    )
-    converted_file_path = utils.resolve_new_converted_file_path(
-        new_format=new_format
-    )
-
-    await voice.download(unconverted_file_path)
-
-    created_file_path = FileConverter.convert_file(
-        input_path=unconverted_file_path,
-        output_path=converted_file_path
-    )
-
-    # Delete files after exiting context manager
-    with utils.hold_files_temporarily(converted_file_path, unconverted_file_path):
-        text = SpeechConverter.audio_to_text(
-            filename=created_file_path, language="en-US"
-        )
+    text = await utils.retrieve_text_from_voice(voice)
 
     await message.reply(text)
 
--- bot/utils.py
+++ bot/utils.py
@@ -1,12 +1,15 @@
 import os
-import typing
 from contextlib import contextmanager
 from pathlib import Path
+
+import aiogram.types
 
 from config import CONVERTED_VOICE_FILES_PATH
 from config import DEFAULT_CONVERTED_FILENAME
 from config import DEFAULT_UNCONVERTED_FILENAME
 from config import UNCONVERTED_VOICE_FILES_PATH
+from file_converter.file_converter import FileConverter
+from speech_converter.speech_converter import SpeechConverter
 
 
 def _resolve_name_for_new_unconverted_file() -> str:
@@ -30,7 +33,7 @@ def _resolve_name_for_new_converted_file() -> str:
 
 
 @contextmanager
-def hold_files_temporarily(*args: typing.Iterable[Path | str]):
+def hold_files_temporarily(*args: Path | str):
     yield
     for path in args:
         if isinstance(path, Path):
@@ -39,15 +42,42 @@ def hold_files_temporarily(*args: typing.Iterable[Path | str]):
         os.remove(path)
 
 
-def resolve_new_converted_file_path(new_format: str):
+def resolve_new_converted_file_path(new_format: str) -> Path:
     name_for_converted_file = _resolve_name_for_new_converted_file()
     converted_file_path = CONVERTED_VOICE_FILES_PATH / \
         (name_for_converted_file + new_format)
     return converted_file_path
 
 
-def resolve_new_unconverted_file_path(new_format: str):
+def resolve_new_unconverted_file_path(new_format: str) -> Path:
     name_for_unconverted_file = _resolve_name_for_new_unconverted_file()
     unconverted_file_path = UNCONVERTED_VOICE_FILES_PATH / \
         (name_for_unconverted_file + new_format)
     return unconverted_file_path
+
+
+async def retrieve_text_from_voice(voice: aiogram.types.Voice) -> str:
+    # Convert to aiff so file can be read by speech-recognition
+    new_format = ".aiff"
+
+    unconverted_file_path = resolve_new_unconverted_file_path(
+        new_format=new_format
+    )
+    converted_file_path = resolve_new_converted_file_path(
+        new_format=new_format
+    )
+
+    await voice.download(unconverted_file_path)
+
+    created_file_path = FileConverter.convert_file(
+        input_path=unconverted_file_path,
+        output_path=converted_file_path
+    )
+
+    # Delete files after exiting context manager
+    with hold_files_temporarily(converted_file_path, unconverted_file_path):
+        text = SpeechConverter.audio_to_text(
+            filename=created_file_path, language="en-US"
+        )
+
+    return text
